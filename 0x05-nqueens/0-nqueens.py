6bebd1d243cb5bee1fa42ead39c20400e87a86a3
Haile Fikere <hailefikere17@gmail.com>
2023-02-02 14:55:20 +0300

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,25 +21,26 @@ if __name__ == '__main__':
         exit(1)
 
     result = []
-    the_queens = []
+    the_queens = []  # coordinates format [row, column]
 
     stop = False
     h = 0
     f = 0
 
-    # iterate rows 
+    #  iterate rows
     while h < n:
         back = False
+        #  iterate column
         while f < n:
 
-    #check current column safty
+            #  check current column safty
             safe = True
             for cord in the_queens:
                 col = cord[1]
-                if(col == c or col + (r- cord[0]) == c or
-                   col - (r-cord[0]) == c)
-                    safe = False
-                    break
+                if(col == f or col + (h- cord[0]) == f or
+                    col - (h-cord[0]) == f)
+                safe = False
+                break
             if not safe:
                 if f == n - 1:
                     back = True
@@ -48,6 +49,45 @@ if __name__ == '__main__':
                 continue
 
             # place queen
-            
-                
-
+            cords = [h, f]
+            the_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if h == n - 1:
+                result.append(the_queens[:])
+                for cord in the_queens:
+                    if cord[1] < n - 1:
+                        h = cord[0]
+                        f = cord[1]
+                for i in range(n - r):
+                    the_queens.pop()
+                if h == n - 1 and f == n - 1:
+                    the_queens = []
+                    stop = True
+                h -= 1
+                f += 1
+            else:
+                f = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if back:
+            h -= 1
+            while h >= 0:
+                f = the_queens[f][1] + 1
+                del the_queens[h] # delete previoous queen coordinates
+                if f < n:
+                    break
+                h -= 1
+            if h < 0:
+                break
+            continue
+        h += 1
+    
+    for idx, val in enumerate(result):
+        if idx == len(result) - 1:
+            print(val, end='')
+        else:
+            print(val)
