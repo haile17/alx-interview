c7c2acfa83de554da922dc56031871699b92f66e
Haile Fikere <hailefikere17@gmail.com>
2023-02-18 10:40:15 +0300

    sw

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films' + movieId;
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 let people = [];
 const names = [];
 
 const requestCharacters = async () => {
   await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
     if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, ' | StatusCode: ', res.statusCode);
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
     } else {
       const jsonBody = JSON.parse(body);
       people = jsonBody.characters;
@@ -22,18 +22,18 @@ const requestCharacters = async () => {
 const requestNames = async () => {
   if (people.length > 0) {
     for (const p of people) {
-      await new Promise(resolve => request (p,(err, res, body) => {
+      await new Promise(resolve => request(p, (err, res, body) => {
         if (err || res.statusCode !== 200) {
-          console.error('Erroe: ', err, '| StatusCode: ', res.statusCode);
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
         } else {
-        const jsonBody = JSON.parse(body);
-        names.push(jsonBody.name);
-        resolve();
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
         }
       }));
     }
   } else {
-    console.error('Erroe: Got no characters for some reason');
+    console.error('Error: Got no Characters for some reason');
   }
 };
 
